79f1daa5444fca219262223eced5180789d95b3e
yangg <uedsky@gmail.com>
2012-04-28 11:49:42 +0800

    rename2

--- find2.el
+++ find2.el
@@ -1,154 +1,146 @@
-;;; lookup-file.el --- A file finder for Emacs
+;;; find2.el --- A file finder for Emacs
 
 ;; Author: uedsky
 ;; Keywords: file finder
 
-(defvar lookup-dir "~")
+(defvar find2-dir "~")
 
-(defvar lookup-omit-extensions ["jpg" "gif" "png" "log" "localized" "DS_Store"])
+(defvar find2-omit-extensions ["jpg" "gif" "png" "log" "localized" "DS_Store"])
 
-(defvar lookup-omit-files [".git" ".svn" ".hg" "images" "log"])
+(defvar find2-omit-files [".git" ".svn" ".hg" "images" "log"])
 
-(defvar lookup-omit-regexp nil)
+(defvar find2-omit-regexp nil)
 
-(defvar lookup-command "find %s -type f"
+(defvar find2-command "find %s -type f"
   "Available values are:
 find %s -type f
-(cd %s && git ls-files)
-locate %s -l9999
+(cd %s && git ls-files)")
 
-create or update locate db in osx:
-sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
-sudo /usr/libexec/locate.updatedb
-or in ubuntu
-sudo updatedb
-")
+(defvar find2-completion-buffer-name "*Find2 Completions*")
 
-(defvar lookup-completion-buffer-name "*Lookup Completions*")
-
-(defun lookup-file-get-list()
+(defun find2-get-list()
   (interactive)
-  (setq lookup-dir (expand-file-name lookup-dir)
-        lookup-filelist [])
-  (let ((output (shell-command-to-string (format lookup-command lookup-dir)))
-        (omit-extensions (concat "\\.\\(" (mapconcat 'regexp-quote lookup-omit-extensions "\\|") "\\)$"))
-        (omit-files (concat "\\(/\\|^\\)\\(" (mapconcat 'regexp-quote lookup-omit-files "\\|") "\\)\\(/\\|$\\)"))
+  (setq find2-dir (expand-file-name find2-dir)
+        find2list [])
+  (let ((output (shell-command-to-string (format find2-command find2-dir)))
+        (omit-extensions (concat "\\.\\(" (mapconcat 'regexp-quote find2-omit-extensions "\\|") "\\)$"))
+        (omit-files (concat "\\(/\\|^\\)\\(" (mapconcat 'regexp-quote find2-omit-files "\\|") "\\)\\(/\\|$\\)"))
         (filename nil))
     (dolist (path (split-string output "\n"))
       (or (string-match omit-files path)
           (string-match omit-extensions path)
-          (and lookup-omit-regexp (string-match lookup-omit-regexp path))
+          (and find2-omit-regexp (string-match find2-omit-regexp path))
           (setq filename (file-name-nondirectory path)
-                lookup-filelist (vconcat lookup-filelist (vector (list path filename))))
+                find2list (vconcat find2list (vector (list path filename))))
           ))))
 
-(defun lookup-quote (str)
+(defun find2-quote (str)
   (replace-regexp-in-string "\\\\\\*" "[^/]*"
                             (replace-regexp-in-string "\\\\\\*\\\\\\*" ".*" (regexp-quote str))))
 
-(defun lookup-command-hook ()
-  (let* ((query (lookup-quote (minibuffer-contents-no-properties)))
+(defun find2-command-hook ()
+  (let* ((query (find2-quote (minibuffer-contents-no-properties)))
         (only-file (if (or (search "/" query) (search ".*" query)) 0 1))
         (filelist []))
-    (get-buffer-create lookup-completion-buffer-name)
-    (set-buffer lookup-completion-buffer-name)
+    (get-buffer-create find2-completion-buffer-name)
+    (set-buffer find2-completion-buffer-name)
     (erase-buffer)
     (mapc '(lambda(file)
              (let ((path (elt file 0)))
                (when (string-match query (elt file only-file))
                  (setq filelist (append filelist (list path)))
-                 ))) lookup-filelist)
+                 ))) find2list)
     (insert (mapconcat 'identity filelist "\n"))
-    (setq lookup-file-selected-index nil)
+    (setq find2-selected-index nil)
     (goto-char (point-min))
     (if (eq (point-min) (point-max))
         (insert "No matched file!")
-      (setq lookup-file-selected-index 1)
-      (lookup-file-highlight))
-    (display-buffer lookup-completion-buffer-name)))
+      (setq find2-selected-index 1)
+      (find2-highlight))
+    (display-buffer find2-completion-buffer-name)))
 
-(setq lookup-file-initialized nil)
-(defun lookup-init ()
-  (unless lookup-file-initialized
-    (setq lookup-file-keymap (make-sparse-keymap))
-    (set-keymap-parent lookup-file-keymap minibuffer-local-map)
-    (define-key lookup-file-keymap "\C-n" 'lookup-file-next)
-    (define-key lookup-file-keymap "\C-p" 'lookup-file-previous)
-    (define-key lookup-file-keymap [down] 'lookup-file-next)
-    (define-key lookup-file-keymap [up] 'lookup-file-previous)
-    (define-key lookup-file-keymap "\C-r" 'lookup-file-get-list)
-    (define-key lookup-file-keymap "\r" 'lookup-file-select)
+(setq find2-initialized nil)
+(defun find2-init ()
+  (unless find2-initialized
+    (setq find2-keymap (make-sparse-keymap))
+    (set-keymap-parent find2-keymap minibuffer-local-map)
+    (define-key find2-keymap "\C-n" 'find2-next)
+    (define-key find2-keymap "\C-p" 'find2-previous)
+    (define-key find2-keymap [down] 'find2-next)
+    (define-key find2-keymap [up] 'find2-previous)
+    (define-key find2-keymap "\C-r" 'find2-get-list)
+    (define-key find2-keymap "\r" 'find2-select)
 
-    (lookup-file-get-list)
-    (setq lookup-file-initialized t)))
+    (find2-get-list)
+    (setq find2-initialized t)))
 
 ;;;###autoload
-(defun lookup-file ()
+(defun find2 ()
   (interactive)
 
-  (lookup-init)
-  (setq lookup-file-mode t)
-  (add-hook 'minibuffer-setup-hook 'lookup-minibuffer-setup)
-  (add-hook 'minibuffer-exit-hook 'lookup-minibuffer-exit)
-  (read-string "Lookup file: ")
-  (if lookup-selected-file
-      (find-file lookup-selected-file)))
+  (find2-init)
+  (setq find2-mode t)
+  (add-hook 'minibuffer-setup-hook 'find2-minibuffer-setup)
+  (add-hook 'minibuffer-exit-hook 'find2-minibuffer-exit)
+  (read-string ">>> ")
+  (if find2-selected-file
+      (find-file find2-selected-file)))
 
-(defun lookup-file-next ()
+(defun find2-next ()
   (interactive)
-  (lookup-file-highlight 1))
+  (find2-highlight 1))
 
-(defun lookup-file-previous ()
+(defun find2-previous ()
   (interactive)
-  (lookup-file-highlight -1))
+  (find2-highlight -1))
 
-(setq lookup-selected-file nil)
-(defun lookup-file-select ()
+(setq find2-selected-file nil)
+(defun find2-select ()
   (interactive)
-  (when lookup-file-selected-index
-    (set-buffer lookup-completion-buffer-name)
+  (when find2-selected-index
+    (set-buffer find2-completion-buffer-name)
     (goto-char (line-beginning-position))
     (delete-char 2)
-    (setq lookup-selected-file (lookup-getline))
-    (unless (string-match "^/\\|^[a-z]:" lookup-selected-file)
-      (setq lookup-selected-file (concat (file-name-as-directory lookup-dir) lookup-selected-file)))
+    (setq find2-selected-file (find2-getline))
+    (unless (string-match "^/\\|^[a-z]:" find2-selected-file)
+      (setq find2-selected-file (concat (file-name-as-directory find2-dir) find2-selected-file)))
     (exit-minibuffer)))
 
-(defun lookup-getline ()
+(defun find2-getline ()
   (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
 
-(defun lookup-file-highlight (&optional delta)
-  (when lookup-file-selected-index
+(defun find2-highlight (&optional delta)
+  (when find2-selected-index
     ;; (save-excursion
-    (set-buffer lookup-completion-buffer-name)
+    (set-buffer find2-completion-buffer-name)
     (when delta
-      (goto-line lookup-file-selected-index)
+      (goto-line find2-selected-index)
       (goto-char (line-beginning-position))
       (delete-char 2)
       (remove-text-properties (line-beginning-position) (line-end-position) '(face nil))
-      (setq lookup-file-selected-index (+ lookup-file-selected-index delta))
+      (setq find2-selected-index (+ find2-selected-index delta))
       (let ((lines-count (count-lines (point-min) (point-max))))
-        (if (< lookup-file-selected-index 1)
-            (setq lookup-file-selected-index (+ lines-count lookup-file-selected-index)))
-        (if (> lookup-file-selected-index lines-count)
-            (setq lookup-file-selected-index (- lookup-file-selected-index lines-count))))
-      (goto-line lookup-file-selected-index)
-      (set-window-point (get-buffer-window lookup-completion-buffer-name) (point)))
+        (if (< find2-selected-index 1)
+            (setq find2-selected-index (+ lines-count find2-selected-index)))
+        (if (> find2-selected-index lines-count)
+            (setq find2-selected-index (- find2-selected-index lines-count))))
+      (goto-line find2-selected-index)
+      (set-window-point (get-buffer-window find2-completion-buffer-name) (point)))
     (goto-char (line-beginning-position))
     (insert "> ")
     (add-text-properties (line-beginning-position) (line-end-position) '(face highlight))
     ;; )
     ))
 
-(defun lookup-minibuffer-setup ()
-  (when lookup-file-mode
-    (add-hook 'post-command-hook 'lookup-command-hook nil t)
-    (use-local-map lookup-file-keymap)
+(defun find2-minibuffer-setup ()
+  (when find2-mode
+    (add-hook 'post-command-hook 'find2-command-hook nil t)
+    (use-local-map find2-keymap)
     ))
 
-(defun lookup-minibuffer-exit ()
-  (when lookup-file-mode
-    (use-local-map (keymap-parent lookup-file-keymap))
-    (setq lookup-file-mode nil)))
+(defun find2-minibuffer-exit ()
+  (when find2-mode
+    (use-local-map (keymap-parent find2-keymap))
+    (setq find2-mode nil)))
 
-(provide 'lookup-file)
+(provide 'find2)
